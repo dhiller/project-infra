910bb0ae341570b440b0f44dadeafb4bfde5059b
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-07-02 18:29:35 +0200

    WIP: expose junit xml
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- robots/flakefinder/flakefinder_suite_test.go
+++ robots/flakefinder/flakefinder_suite_test.go
@@ -7,21 +7,29 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"github.com/onsi/ginkgo/reporters"
 )
 
 type options struct {
 	printTestOutput bool
+	junitOutput     string
 }
 
 var testOptions = options{}
 
 func TestMain(m *testing.M) {
 	flag.BoolVar(&testOptions.printTestOutput, "print_test_output", false, "Whether test output should be printed via logger")
+	flag.StringVar(&testOptions.junitOutput, "junit-output", "", "Set path to Junit report.")
 	flag.Parse()
 	os.Exit(m.Run())
 }
 
 func TestFlakefinder(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Flakefinder Suite")
+	testReporters := []Reporter{}
+	if testOptions.junitOutput != "" {
+		testReporters = append(testReporters, reporters.NewJUnitReporter(testOptions.junitOutput))
+	}
+	RunSpecsWithDefaultAndCustomReporters(t, "Flakefinder Suite", testReporters)
 }
