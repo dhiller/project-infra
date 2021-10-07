abb88b6ce4d13bded6b998beb08081852fe3af40
Daniel Hiller <dhiller@redhat.com>
2021-10-07 18:57:33 +0200

    Write presubmit job status to README in job folder
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

---
+++ github/ci/prow-deploy/files/jobs/kubevirt/kubevirt/README.md
@@ -0,0 +1,32 @@
+
+Presubmit Jobs
+-------------
+
+| Job Name | always_run | optional | skip_report |
+| -------- |: ---------- :|: -------- :|: ----------- :|
+| pull-kubevirt-e2e-k8s-1.22-sig-storage | ✅ | ✅ | ❎ |
+| pull-kubevirt-e2e-k8s-1.22-sig-network | ✅ | ✅ | ❎ |
+| pull-kubevirt-e2e-k8s-1.22-sig-compute-realtime | ❎ | ✅ | ❎ |
+| pull-kubevirt-e2e-k8s-1.22-sig-compute | ✅ | ✅ | ❎ |
+| pull-kubevirt-e2e-k8s-1.22-operator | ✅ | ✅ | ❎ |
+| **pull-kubevirt-e2e-k8s-1.21-sig-storage** | ✅ | ❎ | ❎ |
+| **pull-kubevirt-e2e-k8s-1.21-sig-network** | ✅ | ❎ | ❎ |
+| **pull-kubevirt-e2e-k8s-1.21-sig-compute** | ✅ | ❎ | ❎ |
+| pull-kubevirt-e2e-k8s-1.21-rest-coverage | ❎ | ✅ | ❎ |
+| **pull-kubevirt-e2e-k8s-1.21-operator** | ✅ | ❎ | ❎ |
+| pull-kubevirt-e2e-k8s-1.20-sig-storage-nonroot | ❎ | ✅ | ❎ |
+| pull-kubevirt-e2e-k8s-1.20-sig-storage | ❎ | ✅ | ❎ |
+| pull-kubevirt-e2e-k8s-1.20-sig-network-nonroot | ❎ | ✅ | ❎ |
+| **pull-kubevirt-e2e-k8s-1.20-sig-network** | ✅ | ❎ | ❎ |
+| **pull-kubevirt-e2e-k8s-1.20-sig-compute-nonroot** | ✅ | ❎ | ❎ |
+| **pull-kubevirt-e2e-k8s-1.20-sig-compute** | ✅ | ❎ | ❎ |
+| **pull-kubevirt-e2e-k8s-1.20-operator-nonroot** | ✅ | ❎ | ❎ |
+| **pull-kubevirt-e2e-k8s-1.20-operator** | ✅ | ❎ | ❎ |
+| pull-kubevirt-e2e-k8s-1.20-cgroupsv2-rook-ceph | ❎ | ✅ | ❎ |
+| pull-kubevirt-e2e-k8s-1.20-cgroupsv2 | ❎ | ✅ | ❎ |
+| pull-kubevirt-e2e-k8s-1.19-sig-storage | ❎ | ✅ | ❎ |
+| pull-kubevirt-e2e-k8s-1.19-sig-network | ❎ | ❎ | ❎ |
+| pull-kubevirt-e2e-k8s-1.19-sig-compute | ❎ | ❎ | ❎ |
+| pull-kubevirt-e2e-k8s-1.19-operator | ❎ | ❎ | ❎ |
+
+**Note:** jobs required to pass for merging with tide are printed in **bold**
--- robots/pkg/kubevirt/cmd/get/jobs.go
+++ robots/pkg/kubevirt/cmd/get/jobs.go
@@ -16,10 +16,17 @@ package get
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"sort"
 	"strings"
+	"text/template"
+
+	"k8s.io/test-infra/prow/config"
 
 	"github.com/spf13/cobra"
+
+	"kubevirt.io/project-infra/robots/pkg/kubevirt/cmd/flags"
 
 	"kubevirt.io/project-infra/robots/pkg/kubevirt/prowjobconfigs"
 
@@ -38,15 +45,16 @@ const (
 	json
 )
 
-var outputTextMapping = map[output]string{
-	markdown: "text",
-	json:     "json",
+var outputTextMapping = map[output][]string{
+	markdown: {"markdown"},
+	json:     {"json"},
 }
 
 type getJobOptions struct {
 	jobConfigPathKubevirtPresubmits string
 	jobConfigPathKubevirtPeriodics  string
 	output                          output
+	outputFile                      string
 }
 
 func (o getJobOptions) Validate() error {
@@ -80,6 +88,7 @@ func GetJobsCommand() *cobra.Command {
 func init() {
 	getJobsCommand.PersistentFlags().StringVar(&getJobsOpts.jobConfigPathKubevirtPresubmits, "job-config-path-kubevirt-presubmits", "", "The path to the kubevirt presubmit job definitions")
 	getJobsCommand.PersistentFlags().StringVar(&getJobsOpts.jobConfigPathKubevirtPeriodics, "job-config-path-kubevirt-periodics", "", "The path to the kubevirt periodic job definitions")
+	getJobsCommand.PersistentFlags().StringVar(&getJobsOpts.outputFile, "output-file", "", "The path to the file where the output is written to")
 	getJobsCommand.PersistentFlags().VarP(
 		enumflag.New(&getJobsOpts.output, "output", outputTextMapping, enumflag.EnumCaseInsensitive),
 		"output", "o",
@@ -87,5 +96,87 @@ func init() {
 }
 
 func run(cmd *cobra.Command, args []string) error {
+	err := flags.ParseFlags(cmd, args, getJobsOpts)
+	if err != nil {
+		return err
+	}
+
+	jobConfigs := map[string]func(io.Writer, *config.JobConfig) error{
+		getJobsOpts.jobConfigPathKubevirtPresubmits: printPresubmitJobsForProviders,
+		getJobsOpts.jobConfigPathKubevirtPeriodics:  printPeriodicJobsForProviders,
+	}
+
+	if getJobsOpts.outputFile == "" {
+		return fmt.Errorf("output-file is required")
+	}
+	file, err := os.OpenFile(getJobsOpts.outputFile, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s for writing: %v", getJobsOpts.outputFile, err)
+	}
+	defer file.Close()
+
+	for jobConfigPath, jobConfigPrintFunc := range jobConfigs {
+		jobConfig, err := config.ReadJobConfig(jobConfigPath)
+		if err != nil {
+			return fmt.Errorf("failed to read jobconfig %s: %v", jobConfigPath, err)
+		}
+
+		if err = jobConfigPrintFunc(file, &jobConfig); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var templatePrintPresubmitJobsForProviders = `
+Presubmit Jobs
+-------------
+
+| Job Name | always_run | optional | skip_report |
+| -------- |: ---------- :|: -------- :|: ----------- :|{{ range $presubmitJob := $.PresubmitJobs }}
+| {{ if $presubmitJob.RequiredToMerge }}**{{ end }}{{ $presubmitJob.Name }}{{ if $presubmitJob.RequiredToMerge }}**{{ end }} | {{ if $presubmitJob.Job.AlwaysRun }}✅{{ else }}❎{{ end }} | {{ if $presubmitJob.Job.Optional }}✅{{ else }}❎{{ end }} | {{ if $presubmitJob.Job.SkipReport }}✅{{ else }}❎{{ end }} |{{ end }}
+
+**Note:** jobs required to pass for merging with tide are printed in **bold**
+`
+
+type PresubmitJobs struct {
+	PresubmitJobs []PresubmitJob
+}
+
+type PresubmitJob struct {
+	Name            string
+	RequiredToMerge bool
+	Job             config.Presubmit
+}
+
+func printPresubmitJobsForProviders(wr io.Writer, jobConfig *config.JobConfig) error {
+	kubevirtE2EPresubmitNames := []string{}
+	kubevirtE2EPresubmits := map[string]*config.Presubmit{}
+	for index := range jobConfig.PresubmitsStatic[prowjobconfigs.OrgAndRepoForJobConfig] {
+		job := jobConfig.PresubmitsStatic[prowjobconfigs.OrgAndRepoForJobConfig][index]
+		if !strings.HasPrefix(job.Name, "pull-kubevirt-e2e-k8s-") {
+			continue
+		}
+		kubevirtE2EPresubmitNames = append(kubevirtE2EPresubmitNames, job.Name)
+		kubevirtE2EPresubmits[job.Name] = &job
+	}
+
+	sort.Sort(sort.Reverse(sort.StringSlice(kubevirtE2EPresubmitNames)))
+
+	parse, err := template.New("presubmits").Parse(templatePrintPresubmitJobsForProviders)
+	if err != nil {
+		return err
+	}
+
+	data := PresubmitJobs{}
+	for _, jobName := range kubevirtE2EPresubmitNames {
+		presubmitJob := *kubevirtE2EPresubmits[jobName]
+		requiredToMerge := !presubmitJob.Optional && presubmitJob.AlwaysRun && !presubmitJob.SkipReport
+		data.PresubmitJobs = append(data.PresubmitJobs, PresubmitJob{jobName, requiredToMerge, presubmitJob})
+	}
+	return parse.Execute(wr, data)
+}
+
+func printPeriodicJobsForProviders(wr io.Writer, jobConfig *config.JobConfig) error {
 	return nil
 }
